729c94230555ba1507663bba0b71ebafc45b3a8e
Daniel Moberg <daniel@moberg.no>
2018-02-24 22:26:50 +0100

    workout edit

--- app/src/main/java/com/boyz/code/workouttimer/WorkoutActivity.kt
+++ app/src/main/java/com/boyz/code/workouttimer/WorkoutActivity.kt
@@ -2,9 +2,12 @@ package com.boyz.code.workouttimer
 
 import android.app.Activity
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.LinearLayout
 import com.boyz.code.workouttimer.misc.Workout
 import com.boyz.code.workouttimer.misc.WorkoutItem
@@ -25,8 +28,24 @@ class WorkoutActivity : Activity() {
         
         val title = intent.getStringExtra("title")
         workoutItems += WorkoutManager.getWorkout(this, title).items
+        setTitle("Workout: " + title)
 
         var adapter = WorkoutItemAdapter(workoutItems)
         rv.adapter = adapter
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.workout_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return when (item!!.itemId) {
+            R.id.workoutMenuOptions -> {
+                startActivity(Intent(this, WorkoutEditActivity::class.java))
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
