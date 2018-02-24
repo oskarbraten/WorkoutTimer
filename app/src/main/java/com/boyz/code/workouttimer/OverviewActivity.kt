cf3ad53594be4ccb83cd14ff22003286c9f7237f
oskarbraten <oskar.braten@outlook.com>
2018-02-24 13:44:51 +0100

    Initial commit

---
+++ app/src/main/java/com/boyz/code/workouttimer/OverviewActivity.kt
@@ -0,0 +1,12 @@
+package com.boyz.code.workouttimer
+
+import android.app.Activity
+import android.os.Bundle
+
+class OverviewActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_overview)
+    }
+}
