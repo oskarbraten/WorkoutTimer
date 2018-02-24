830c7de91ad1622de843e6d0af834f71fa7d193b
Daniel Moberg <daniel@moberg.no>
2018-02-24 14:51:17 +0100

    RecyclerView v2

--- app/src/main/java/com/boyz/code/workouttimer/OverviewActivity.kt
+++ app/src/main/java/com/boyz/code/workouttimer/OverviewActivity.kt
@@ -7,6 +7,8 @@ import android.support.v7.widget.RecyclerView
 import android.widget.LinearLayout
 import com.boyz.code.workouttimer.misc.CustomAdapter
 import com.boyz.code.workouttimer.misc.Workout
+import com.boyz.code.workouttimer.misc.WorkoutItem
+import kotlinx.android.synthetic.main.activity_overview.*
 
 class OverviewActivity : Activity() {
 
@@ -14,14 +16,20 @@ class OverviewActivity : Activity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_overview)
 
-        val cards = ArrayList<Workout>()
-
-//        cards.add(Workout("hehe", 200))
+        val workouts = ArrayList<Workout>()
 
         val rv = findViewById<RecyclerView>(R.id.recyclerViewWorkout)
         rv.layoutManager = LinearLayoutManager(this, LinearLayout.VERTICAL, false)
 
-        var adapter = CustomAdapter(cards)
+        var adapter = CustomAdapter(workouts)
         rv.adapter = adapter
+
+        addWorkoutButton.setOnClickListener {
+            val workoutItems = ArrayList<WorkoutItem>()
+            workoutItems.add(WorkoutItem("PushUps", 1000))
+            workoutItems.add(WorkoutItem("Pause", 1000))
+            workouts.add(Workout("Test", workoutItems))
+            adapter.notifyDataSetChanged()
+        }
     }
 }
--- app/src/main/java/com/boyz/code/workouttimer/misc/Workout.kt
+++ app/src/main/java/com/boyz/code/workouttimer/misc/Workout.kt
@@ -2,7 +2,7 @@ package com.boyz.code.workouttimer.misc
 
 data class WorkoutItem(var title: String, var length: Int)
 
-data class Workout(var title: String, val items : List<WorkoutItem>) {
+data class Workout(var title: String, val items : ArrayList<WorkoutItem>) {
     fun length(): Int {
         return items.fold(0) { total, next -> total + next.length }
     }
