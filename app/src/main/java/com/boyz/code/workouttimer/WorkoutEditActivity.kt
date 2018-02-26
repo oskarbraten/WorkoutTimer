2fc98a432f1df6e9d23773e1008ce0347cd90485
Daniel Moberg <daniel@moberg.no>
2018-02-26 15:32:39 +0100

    add og edit exercise dialog builder

--- app/src/main/java/com/boyz/code/workouttimer/WorkoutEditActivity.kt
+++ app/src/main/java/com/boyz/code/workouttimer/WorkoutEditActivity.kt
@@ -7,20 +7,14 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
-import android.text.Editable
-import android.text.TextWatcher
-import android.util.Log
 import android.view.LayoutInflater
 import android.widget.LinearLayout
 import android.widget.Toast
 import com.boyz.code.workouttimer.data.Exercise
 import com.boyz.code.workouttimer.data.Workout
-import com.boyz.code.workouttimer.misc.ExerciseEditAdapter
-import com.boyz.code.workouttimer.misc.WorkoutManager
+import com.boyz.code.workouttimer.misc.*
 import kotlinx.android.synthetic.main.activity_workout_edit.*
-import kotlinx.android.synthetic.main.add_exercise_dialog.*
 import kotlinx.android.synthetic.main.add_exercise_dialog.view.*
-import kotlinx.android.synthetic.main.add_workout_dialog.view.*
 
 class WorkoutEditActivity : Activity() {
 
@@ -72,25 +66,11 @@ class WorkoutEditActivity : Activity() {
 
             alertDialogBuilder.setView(promptsView)
 
-            promptsView.minutesInput.setOnFocusChangeListener { view, b ->
-                if (!b && !promptsView.minutesInput.text.isNullOrEmpty()) {
-                    when (promptsView.minutesInput.text.toString().toInt()) {
-                        in 0..9 -> promptsView.minutesInput.setText("0" + promptsView.minutesInput.text.toString())
-                    }
-                }
-            }
-
-            promptsView.secondsInput.setOnFocusChangeListener { view, b ->
-                if (!b && !promptsView.secondsInput.text.isNullOrEmpty()) {
-                    when (promptsView.secondsInput.text.toString().toInt()) {
-                        in 0..9 -> promptsView.secondsInput.setText("0" + promptsView.secondsInput.text.toString())
-                    }
-                }
-            }
-
             val exerciseTitleInput = promptsView.exerciseTitleInput
             val minutesInput = promptsView.minutesInput
             val secondsInput = promptsView.secondsInput
+            val includeTimerSwitcher = promptsView.includeTimerSwitcher
+            val durationWrapper = promptsView.durationWrapper
 
             alertDialogBuilder.setCancelable(false)
             alertDialogBuilder.setPositiveButton("OK", { dialogInterface: DialogInterface, i: Int ->
@@ -119,80 +99,8 @@ class WorkoutEditActivity : Activity() {
             alertDialog.show()
             alertDialog.getButton(AlertDialog.BUTTON_POSITIVE)?.isEnabled = false
 
-            fun isInputsValid(): Boolean {
-                if (promptsView.includeTimerSwitcher.isChecked) {
-                    return (!promptsView.exerciseTitleInput.text.toString().equals(""))
-                            && (((!minutesInput.text.toString().equals(""))
-                            && (!minutesInput.text.toString().equals("00"))
-                            && ((!minutesInput.text.toString().equals("0"))))
-                            || ((!secondsInput.text.toString().equals(""))
-                            && (!secondsInput.text.toString().equals("00"))
-                            && ((!secondsInput.text.toString().equals("0")))))
-                } else {
-                    return (!promptsView.exerciseTitleInput.text.toString().equals(""))
-                }
-            }
+            buildAddEditDialog(exerciseTitleInput, includeTimerSwitcher, minutesInput, secondsInput, durationWrapper, alertDialog)
 
-            promptsView.includeTimerSwitcher.setOnCheckedChangeListener { compoundButton, isChecked ->
-                if (isChecked) {
-                    promptsView.durationWrapper.visibility = LinearLayout.VISIBLE
-                } else {
-                    promptsView.durationWrapper.visibility = LinearLayout.GONE
-                }
-
-                alertDialog.getButton(AlertDialog.BUTTON_POSITIVE)?.isEnabled = isInputsValid()
-            }
-
-            promptsView.exerciseTitleInput.addTextChangedListener(object : TextWatcher {
-                override fun afterTextChanged(p0: Editable?) {
-                }
-
-                override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                }
-
-                override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                    alertDialog.getButton(AlertDialog.BUTTON_POSITIVE)?.isEnabled = isInputsValid()
-                }
-            })
-
-            promptsView.minutesInput.addTextChangedListener(object : TextWatcher {
-                override fun afterTextChanged(p0: Editable?) {
-
-                }
-
-                override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-
-                }
-
-                override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                    if (p0?.length!! > 1) {
-                        promptsView.secondsInput.requestFocus()
-                    }
-
-                    if (p0.toString().isNullOrEmpty()) {
-                        promptsView.minutesInput.setText("00")
-                    }
-
-                    alertDialog.getButton(AlertDialog.BUTTON_POSITIVE)?.isEnabled = isInputsValid()
-                }
-            })
-
-            promptsView.secondsInput.addTextChangedListener(object : TextWatcher {
-                override fun afterTextChanged(p0: Editable?) {
-                }
-
-                override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                }
-
-                override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                    if (p0.toString().isNullOrEmpty()) {
-                        promptsView.minutesInput.requestFocus()
-                        promptsView.secondsInput.setText("00")
-                    }
-
-                    alertDialog.getButton(AlertDialog.BUTTON_POSITIVE)?.isEnabled = isInputsValid()
-                }
-            })
         }
     }
 
--- app/src/main/java/com/boyz/code/workouttimer/misc/addEditExerciseDialogBuilder.kt
+++ app/src/main/java/com/boyz/code/workouttimer/misc/addEditExerciseDialogBuilder.kt
@@ -1,7 +1,103 @@
 package com.boyz.code.workouttimer.misc
 
-import android.view.View
+import android.app.AlertDialog
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
+import android.widget.LinearLayout
+import android.widget.Switch
 
-fun View?.buildaddEditDialog() {
+fun buildAddEditDialog(exerciseTitleInput: EditText, includeTimerSwitcher: Switch, minutesInput: EditText, secondsInput: EditText, durationWrapper: LinearLayout, alertDialog: AlertDialog){
+
+    minutesInput.setOnFocusChangeListener { view, b ->
+        if (!b && !minutesInput.text.isNullOrEmpty()) {
+            when (minutesInput.text.toString().toInt()) {
+                in 0..9 -> minutesInput.setText("0" + minutesInput.text.toString())
+            }
+        }
+    }
+
+    secondsInput.setOnFocusChangeListener { view, b ->
+        if (!b && !secondsInput.text.isNullOrEmpty()) {
+            when (secondsInput.text.toString().toInt()) {
+                in 0..9 -> secondsInput.setText("0" + secondsInput.text.toString())
+            }
+        }
+    }
+
+    fun isInputsValid(): Boolean {
+        if (includeTimerSwitcher.isChecked) {
+            return (!exerciseTitleInput.text.toString().equals(""))
+                    && (((!minutesInput.text.toString().equals(""))
+                    && (!minutesInput.text.toString().equals("00"))
+                    && ((!minutesInput.text.toString().equals("0"))))
+                    || ((!secondsInput.text.toString().equals(""))
+                    && (!secondsInput.text.toString().equals("00"))
+                    && ((!secondsInput.text.toString().equals("0")))))
+        } else {
+            return (!exerciseTitleInput.text.toString().equals(""))
+        }
+    }
+
+    includeTimerSwitcher.setOnCheckedChangeListener { compoundButton, isChecked ->
+        if (isChecked) {
+            durationWrapper.visibility = LinearLayout.VISIBLE
+        } else {
+            durationWrapper.visibility = LinearLayout.GONE
+        }
+
+        alertDialog.getButton(AlertDialog.BUTTON_POSITIVE)?.isEnabled = isInputsValid()
+    }
+
+    exerciseTitleInput.addTextChangedListener(object : TextWatcher {
+        override fun afterTextChanged(p0: Editable?) {
+        }
+
+        override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+        }
+
+        override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            alertDialog.getButton(AlertDialog.BUTTON_POSITIVE)?.isEnabled = isInputsValid()
+        }
+    })
+
+    minutesInput.addTextChangedListener(object : TextWatcher {
+        override fun afterTextChanged(p0: Editable?) {
+
+        }
+
+        override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+        }
+
+        override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            if (p0?.length!! > 1) {
+                secondsInput.requestFocus()
+            }
+
+            if (p0.toString().isNullOrEmpty()) {
+                minutesInput.setText("00")
+            }
+
+            alertDialog.getButton(AlertDialog.BUTTON_POSITIVE)?.isEnabled = isInputsValid()
+        }
+    })
+
+    secondsInput.addTextChangedListener(object : TextWatcher {
+        override fun afterTextChanged(p0: Editable?) {
+        }
+
+        override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+        }
+
+        override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            if (p0.toString().isNullOrEmpty()) {
+                minutesInput.requestFocus()
+                secondsInput.setText("00")
+            }
+
+            alertDialog.getButton(AlertDialog.BUTTON_POSITIVE)?.isEnabled = isInputsValid()
+        }
+    })
 
 }
